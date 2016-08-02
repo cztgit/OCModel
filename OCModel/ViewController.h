54c0b4fdd284e9501bac7cbf0daa759a870ba9f2
Tuluobo <tuluobo@vip.qq.com>
2016-08-02 15:52:02 +0800

    init project

---
+++ OCModel/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OCModel
+//
+//  Created by WangHao on 16/8/2.
+//  Copyright © 2016年 Tuluobo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OCModel/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  OCModel
+//
+//  Created by WangHao on 16/8/2.
+//  Copyright © 2016年 Tuluobo. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
