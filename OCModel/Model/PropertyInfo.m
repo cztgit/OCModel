80987286fdd661fd1db3a29efaa3dca472a9cc91
Tuluobo <tuluobo@vip.qq.com>
2016-08-03 11:37:07 +0800

    key note

--- OCModel/Model/PropertyInfo.m
+++ OCModel/Model/PropertyInfo.m
@@ -22,13 +22,15 @@
     unsigned int attrCount;
     objc_property_attribute_t *attrs = property_copyAttributeList(property, &attrCount);
     for (unsigned i=0; i<attrCount; i++) {
-        NSLog(@"attr.name = %s", attrs[i].name);
+        //NSLog(@"%s.name = %s", propertyName, attrs[i].name);
+        //NSLog(@"%s.value = %s", propertyName, attrs[i].value);
         if (attrs[i].name[0] == 'T') {
             size_t len = strlen(attrs[i].value);
             if (len>3) {
                 char name[len-2];
                 name[len-3] = '\0';
                 memcpy(name, attrs[i].value+2, len-3);
+                NSLog(@"%s ===》typeName = %s", propertyName, name);
                 _typeClass = objc_getClass(name);
             }
         }
